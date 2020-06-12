1f2c71e309b5739bb13f69a9e0d2de0ba07ce6a2
vadimkirov <vadimkirov@yandex.ru>
2020-06-12 16:34:24 +0300

    12/06/16/34

--- src/main/java/importdata/ImportCsv.java
+++ src/main/java/importdata/ImportCsv.java
@@ -1,6 +1,8 @@
 package importdata;
 
 import java.io.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -9,51 +11,65 @@ public class ImportCsv {
 
 
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String file = scanner.nextLine();
-        String path = "F:\\dataTestCSV\\";
-//        String fullPath = path+file;
-        lokForDir(',');
-//        sortBySystem(path,fullPath,',');
+
+        String delimiter = uiAnswer("Введите разделитель данных в файле(ах)(пример  ,    :");
+        lokForDir(delimiter.charAt(0));
+
     }
+
+    private static String uiAnswer(String question){
+         String answer="";
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            while (answer.equals("")){
+                System.out.print(question);
+                answer = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return answer;
+    }
+
 
     private static void lokForDir(Character delimiter){
         File directory;
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            // Считываем исходный каталог для поиска файлов.
-            System.out.print("Введите исходную директорию для поиска файлов:");
-            final String directoryPath = reader.readLine();
-            reader.close();
+        // Считываем исходный каталог для поиска файлов.
+        final String directoryPath = uiAnswer("Введите исходную директорию для поиска файлов:");
 
-            directory = new File(directoryPath);
-            // Убедимся, что директория найдена и это реально директория, а не файл.
-            if (directory.exists() && directory.isDirectory()) {
-                processDirectory(directory,delimiter);
-            } else {
-                System.out.println("Не удалось найти директорию по указанному пути.");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        directory = new File(directoryPath);
+        // Убедимся, что директория найдена и это реально директория, а не файл.
+        if (directory.exists() && directory.isDirectory()) {
+            processDirectory(directory,delimiter);
+        } else {
+            System.out.println("Не удалось найти директорию по указанному пути.");
         }
 
     }
 
 
-    private static void sortBySystem(String path, String fileName, Character delimiter){
+    private static void sortBySystem(String path, String fileName, Character delimiter, Map<String, CurrentProcessingData> mapSortFiles){
         {
+//           создать папку для хранения временных файлов
+
             try
             {
                 //здесь "sleep 15" и есть ваша консольная команда
+
                 // -t, - если делитель - запятая
 
 //          sort.exe yourfile.csv -t, -k5 -g -o sorted.csv
                 String outFileName = fileName.substring(0,fileName.indexOf('.') ) + "sort.csv";
 
-                String command = path+"\\sort.exe "+ fileName + " -t" + delimiter + " -k5 -g -o " + outFileName;
+                String command = path+"\\sort.exe "+ fileName + " -t" + delimiter + " -k5 -g -o  " + outFileName; // sort -k5 -g file.csv > sorted.csv для Linux
                 Process proc = Runtime.getRuntime().exec(command);
                 proc.waitFor();
                 proc.destroy();
+                mapSortFiles.put(outFileName, new CurrentProcessingData(0,false,  0.0f));
             }
             catch (IOException | InterruptedException e)
             {
@@ -64,13 +80,10 @@ public class ImportCsv {
 
     private static void processDirectory(final File directory, final Character delimiter) {
         // Получаем список доступных файлов в указанной директории.
-        FilenameFilter filter = new FilenameFilter() {
-            @Override
-            public boolean accept(File f, String name) {
-                return name.endsWith(".csv");
-            }
-        };
+        FilenameFilter filter = (file, name)-> name.endsWith(".csv");
+
         File[] files = directory.listFiles(filter);
+
         if (files == null) {
             System.out.println("Нет доступных файлов для обработки.");
             return;
@@ -78,39 +91,34 @@ public class ImportCsv {
             System.out.println("Количество файлов для обработки: " + files.length);
         }
 
+        int waitTime = Integer.parseInt("Введите ожидаемое время работы программы в минутах:");
+
+        ConcurrentHashMap<String,CurrentProcessingData> mapSortFiles = new ConcurrentHashMap<>();
+
         // Непосредственно многопоточная обработка файлов.
         final int treadCount = Runtime.getRuntime().availableProcessors() + 1; // количество потоков, которые могу запустить
+
         ExecutorService service = Executors.newFixedThreadPool(treadCount);
+
         for (final File f : files) {
-//            String ext = getFileExtension(f);
             if (!f.isFile()) {
                 continue;
             }
-
-            service.execute(new Runnable() {
-                @Override
-                public void run() {
-//                    try (BufferedReader reader = new BufferedReader(new FileReader(f))) {
-//                        int lines = 0;
-//                        while (reader.readLine() != null) {
-//                            ++lines;
-//                        }
-//                        System.out.println("Поток: " + Thread.currentThread().getName() + ". Файл: " + f.getName() + ". Количество строк: " + lines);
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-                 sortBySystem(directory.toString(),f.toString(),delimiter);
-                }
-            });
+            service.execute(() -> sortBySystem(directory.toString(),f.toString(),delimiter, mapSortFiles));
         }
         // Новые задачи более не принимаем, выполняем только оставшиеся.
         service.shutdown();
         // Ждем завершения выполнения потоков не более 10 минут.
         try {
-            service.awaitTermination(10, TimeUnit.MINUTES); //Максимальное время обработки
+            service.awaitTermination(waitTime, TimeUnit.MINUTES); //Максимальное время обработки
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
+
+
+
     }
 
 //    private static void readCSV(String fileName, Character delimiter) {
